81ff4e2d27bd06adbb0b9f1b5428ee06d1781c6f
Adri-shikhar <adrishikharbarua77452@gmail.com>
2026-02-22 15:04:06 +0600

    first commit

---
+++ login.js
@@ -0,0 +1,15 @@
+document.getElementById('loginButton').addEventListener('click', function() {
+   const mobileNumber = document.getElementById('mobile-number').value;
+   const pin = document.getElementById('pin').value;
+
+   if(mobileNumber == '01632311801' && pin == '1234') {
+
+      alert('Login successful!');
+      window.location.assign("/home.html");
+   } 
+   else {
+      alert('Invalid mobile number or PIN.');
+   }
+
+   
+});
